d96ac83d32f374fce93c8c24c294f56772a8e07a
JCayllahuaG <u202019558@upc.edu.pe>
2024-07-08 00:39:55 -0500

    feat: ChipCategory component added to show category chips in each card associated

---
+++ frontend/src/note_management/components/ChipCategory.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue';
+let props = defineProps<{
+  text: string;
+  color: string;
+}>();
+
+let color = ref(props.color);
+const convertHexToRGBA = (hexCode: string, opacity: number) => {
+  hexCode = hexCode.replace('#', '');
+
+  const r = parseInt(hexCode.substring(0, 2), 16);
+  const g = parseInt(hexCode.substring(2, 4), 16);
+  const b = parseInt(hexCode.substring(4, 6), 16);
+
+  const alpha = opacity / 100;
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
+</script>
+<template>
+  <div
+    class="w-fit h-7 mt-2 items-center border rounded-lg py-1.5 px-3 font-sans text-xs font-semibold uppercase text-gray-200"
+    :style="{ backgroundColor: convertHexToRGBA(color, 10), borderColor: props.color }"
+  >
+    <span>{{ props.text }}</span>
+  </div>
+</template>
