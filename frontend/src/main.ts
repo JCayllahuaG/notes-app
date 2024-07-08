80434febc4913fb0cbaa45b9ebf0411fe2f7ce55
JCayllahuaG <u202019558@upc.edu.pe>
2024-07-08 00:43:10 -0500

    fix: unused imports removed

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,14 +5,12 @@ import App from './App.vue'
 import router from './router'
 import NoteCard from './note_management/components/NoteCard.vue'
 import NoteManagement from './note_management/pages/NoteManagement.vue'
-import NotePanel from './note_management/components/NotePanel.vue'
 import ChipCategory from './note_management/components/ChipCategory.vue'
 import NoteDialog from './note_management/components/NoteDialog.vue'
 const app = createApp(App);
 app.use(router);
 app.component('note-card', NoteCard);
 app.component('note-management', NoteManagement);
-app.component('note-panel', NotePanel);
 app.component('chip-category', ChipCategory);
 app.component('note-dialog', NoteDialog);
 app.mount('#app');
